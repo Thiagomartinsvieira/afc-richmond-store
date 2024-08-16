104873454a2fc547b4b6f833fbfb8215acd185bc
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-08-15 22:04:24 -0300

    feat(back-end) Listingm, Removing and Editing Status

--- src/main/java/com/project/store/controller/StateController.java
+++ src/main/java/com/project/store/controller/StateController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class StateController {
@@ -21,7 +24,27 @@ public class StateController {
         return mv;
     }
 
-    @PostMapping("saveState")
+    @GetMapping("/listState")
+    public ModelAndView list() {
+        ModelAndView mv = new ModelAndView("administrative/states/list");
+        mv.addObject("listStates", stateRepository.findAll());
+        return mv;
+    }
+
+    @GetMapping("/editState/{id}")
+    public ModelAndView edit(@PathVariable("id") Long id){
+        Optional<State> state = stateRepository.findById(id);
+        return register(state.get());
+    }
+
+    @GetMapping("/removeState/{id}")
+    public ModelAndView remove(@PathVariable("id") Long id){
+        Optional<State> state = stateRepository.findById(id);
+        stateRepository.delete(state.get());
+        return list();
+    }
+
+    @PostMapping("/saveState")
     public ModelAndView save(State state, BindingResult result) {
         if (result.hasErrors()) {
             return register(state);
